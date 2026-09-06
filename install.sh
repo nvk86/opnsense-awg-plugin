f6c1376591d15bba66daff7f1914704c8435a727
Nik <109945253+nvk86@users.noreply.github.com>
2026-09-06 22:20:10 +0300

    Bump plugin installer to 2.0.4

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-# opnsense-awg v2.0.3 installer
+# opnsense-awg v2.0.4 installer
 # Migrates the legacy FreeBSD amnezia-kmod/amnezia-tools AWG2 stack to the
 # project-owned AWG 3.1 packages, preserving OPNsense configuration and keys.
 set -eu
 
-PLUGIN_VERSION="2.0.3"
+PLUGIN_VERSION="2.0.4"
 KMOD_REPO="nvk86/opnsense-awg-kmod"
 TOOLS_REPO="nvk86/opnsense-awg-tools"
 KMOD_VERSION=""
